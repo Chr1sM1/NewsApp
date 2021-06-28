95cec5e0711b838aa3f62c448d009d1351b1ae77
Chr1sM1 <mzs0119@qq.com>
2021-06-28 14:01:21 +0800

    添加聚合数据api，首页加入新闻,可以点击查看详细内容

--- app/src/main/java/com/example/newsapp/fragment/NewsFragment.java
+++ app/src/main/java/com/example/newsapp/fragment/NewsFragment.java
@@ -1,13 +1,16 @@
 package com.example.newsapp.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -15,7 +18,19 @@ import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.newsapp.NewsInfoActivity;
 import com.example.newsapp.R;
+import com.example.newsapp.adapter.NewsAdapter;
+import com.example.newsapp.bean.NewsData;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,6 +46,26 @@ public class NewsFragment extends Fragment {
     private List<ImageView> images;
     private List<View> dots;
     private int currentItem;
+
+    /**
+     * 新闻列表请求接口
+     */
+    public static final String URL = "http://v.juhe.cn/toutiao/index?type=top&key=b70c114114f788be802d976c6f2b631f";
+
+    /**
+     * ListView对象
+     */
+    private ListView listView;
+
+    /**
+     * 新闻集合对象
+     */
+    private List<NewsData> datas;
+
+    /**
+     * 自定义的Adapter对象
+     */
+    private NewsAdapter newsAdapter;
 
     public static NewsFragment newInstance() {
         NewsFragment fragment = new NewsFragment();
@@ -70,8 +105,104 @@ public class NewsFragment extends Fragment {
 
         mView=inflater.inflate(R.layout.fragment_news, null);
         setView();
+        setListView();
         return mView;
     }
+
+    private void setListView() {
+        listView = (ListView) mView.findViewById(R.id.lv_news);
+
+        datas = new ArrayList<NewsData>();
+
+        getDatas(URL);
+
+        /**
+         * 实例化Adapter对象(注意:必须要写在在getDatas() 方法后面,不然datas中没有数据)
+         */
+        newsAdapter = new NewsAdapter(getActivity(), datas);
+    }
+
+    /**
+     * 通过接口获取新闻列表的方法
+     * @param url
+     */
+
+    private void getDatas(String url) {
+        final RequestQueue mQueue = Volley.newRequestQueue(getActivity());
+
+        JsonObjectRequest stringRequest = new JsonObjectRequest(url, null,
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject jsonObject) {
+
+                        try {
+
+                            /**
+                             * 对返回的json数据进行解析,然后装入datas集合中
+                             */
+                            JSONObject jsonObject2 = jsonObject.getJSONObject("result");
+                            JSONArray jsonArray = jsonObject2.getJSONArray("data");
+
+                            for (int i = 0; i <jsonArray.length() ; i++) {
+                                JSONObject item = jsonArray.getJSONObject(i);
+                                NewsData data = new NewsData();
+                                data.setNewsTitle(item.getString("title"));
+                                data.setNewsDate(item.getString("date"));
+                                data.setNewsImgUrl(item.getString("thumbnail_pic_s"));
+                                data.setNewsUrl(item.getString("url"));
+                                datas.add(data);
+                            }
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        /**
+                         * 请求成功后为ListView设置Adapter
+                         */
+                        listView.setAdapter(newsAdapter);
+                        newsAdapter.notifyDataSetChanged();
+
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+
+            }
+        }
+        );
+
+        mQueue.add(stringRequest);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position,
+                                    long arg3) {
+
+                /**
+                 * 创建一个意图
+                 */
+                Intent intent = new Intent(getActivity(), NewsInfoActivity.class);
+
+                /**
+                 * 在datas中通过点击的位置position通过get()方法获得具体某个新闻
+                 * 的数据然后通过Intent的putExtra()传递到NewsInfoActivity中
+                 */
+                intent.putExtra("newsTitle", datas.get(position).getNewsTitle());
+                intent.putExtra("newsDate", datas.get(position).getNewsDate());
+                intent.putExtra("newsImgUrl", datas.get(position).getNewsImgUrl());
+                intent.putExtra("newsUrl", datas.get(position).getNewsUrl());
+
+                startActivity(intent);//启动Activity
+
+            }
+        });
+    }
+
     private void setView(){
         mViewPaper = (ViewPager)mView.findViewById(R.id.vp);
 
@@ -120,6 +251,7 @@ public class NewsFragment extends Fragment {
 
             }
         });
+
     }
 
     /*定义的适配器*/
